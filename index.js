4d03b6c2ce6df83b13db4f692cc2336e5f801bd2
maxhash <ukerest@gmail.com>
2018-02-09 23:32:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import observeCursor from './observe-cursor';
 import autorun from './autorun';
 
 checkNpmVersions({
-  'mobx': '2.x'
+  'mobx': '3.4.x'
 }, 'space:tracker-mobx-autorun');
 
 export default autorun;
